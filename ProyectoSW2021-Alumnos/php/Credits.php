17ef5af0323d0c4b3777f352da93a3658883f78c
Shaitz <udelrio002@ikasle.ehu.eus>
2021-09-18 14:05:09 +0200

    Créditos añadidos

--- ProyectoSW2021-Alumnos/php/Credits.php
+++ ProyectoSW2021-Alumnos/php/Credits.php
@@ -9,6 +9,9 @@
     <div>
 
       <h2>DATOS DEL AUTOR/AUTORES</h2>
+      <h3>Unai del Rio</h3>
+      <h4>Ingeniería del Software</h4>
+      <img src='../images/avatar.png' alt="foto" height="48" width="48" />
 
     </div>
   </section>
