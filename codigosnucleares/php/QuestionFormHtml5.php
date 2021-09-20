581496a31e830776beb717ee151991ab48d7e059
Shaitz <shaitrealm@gmail.com>
2021-09-20 13:46:01 +0000

    required html5

--- codigosnucleares/php/QuestionFormHtml5.php
+++ codigosnucleares/php/QuestionFormHtml5.php
@@ -7,9 +7,40 @@
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
+    <form action="AddQuestionWithImage.php" id="fquestion" name="fquestion" method="post" enctype="multipart/form-data" onsubmit="return verificar()">
+        <label for="email">E-mail<span style="color: #800080">(*)</span>:</label>
+        <input type="text" id="email" name="email" required><br><br>
 
-      Versión HTML 5 del formulario para insertar preguntas.
-	  OPCIONAL.
+        <label for="pregunta">Pregunta<span style="color: #800080">(*)</span>:</label>
+        <input type="text" id="pregunta" name="pregunta" required><br><br>
+
+        <label for="respuestaCorrecta">Respuesta Correcta<span style="color: #800080">(*)</span>:</label>
+        <input type="text" id="respuestaCorrecta" name="respuestaCorrecta" required><br><br>
+
+        <label for="respuestaIncorrecta1">Respuesta Incorrecta 1<span style="color: #800080">(*)</span>:</label>
+        <input type="text" id="respuestaIncorrecta1" name="respuestaIncorrecta1" required><br><br>
+
+        <label for="respuestaIncorrecta2">Respuesta Incorrecta 2<span style="color: #800080">(*)</span>:</label>
+        <input type="text" id="respuestaIncorrecta2" name="respuestaIncorrecta2" required><br><br>
+
+        <label for="respuestaIncorrecta3">Respuesta Incorrecta 3<span style="color: #800080">(*)</span>:</label>
+        <input type="text" id="respuestaIncorrecta3" name="respuestaIncorrecta3" required><br><br>
+
+        <label for="dificultad">Dificultad<span style="color: #800080">(*)</span>:</label>
+        <select id="dificultad" name="dificultad">
+          <option selected="selected" value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option></select><br><br>
+
+        <label for="tema">Tema<span style="color: #800080">(*)</span>:</label>
+        <input type="text" id="tema" name="tema" required><br><br>
+
+        <label for="imagen">Imagen:</label>
+        <input type="file" name="imagen" id="imagen" accept="image/*" onchange="readURL(event)"><br>
+        <img id="output" width="100" /><br><br>
+
+        <input type="submit" value="Enviar" name="enviar">
+      </form>
 
     </div>
   </section>
