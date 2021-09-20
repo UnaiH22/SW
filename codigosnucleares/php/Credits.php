fa65ba08bb220ca36f70f2d5c48085c3794b0a65
Shaitz <shaitrealm@gmail.com>
2021-09-20 13:41:38 +0000

    creditos actualizados

--- codigosnucleares/php/Credits.php
+++ codigosnucleares/php/Credits.php
@@ -11,8 +11,8 @@
       <h2>DATOS DEL AUTOR/AUTORES</h2>
       <h3>Unai del Rio  / Unai Heras</h3>
       <h4>Ingeniería del Software</h4>
-      <img src='../images/avatar.png' alt="foto" height="70" width="70" /><br>
-      <img src='../images/kaladin.jpg' alt="foto2" height="70" width="70" /><br>
+      <img src='../images/avatar.png' alt="foto" height="70" width="70" />
+      <img src='../images/kaladin.jpg' alt="foto2" height="100" width="70" /><br>
 
     </div>
   </section>
