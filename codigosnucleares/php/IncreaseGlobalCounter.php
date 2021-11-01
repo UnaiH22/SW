370f3cae3132f889f3d9bb4906d3a49ae542e684
Shait <shaitrealm@gmail.com>
2021-11-01 13:07:19 +0100

    fix increaseCounter

--- codigosnucleares/php/IncreaseGlobalCounter.php
+++ codigosnucleares/php/IncreaseGlobalCounter.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
+
 if (file_exists('../xml/UserCounter.xml'))
     $xml = simplexml_load_file('../xml/UserCounter.xml');
 else
     die('Error abriendo el xml.');
-        
+            
 if ($xml === FALSE)
     die('Error al procesar el xml.');
 
@@ -17,18 +18,21 @@ foreach($xml->xpath('user') as $us)
     if ($us == "")
         unset($us[0][0]);
 }
-if (!$found)
+if (isset($_REQUEST['addUser']))
 {
-    $xml->addChild('user', $_SESSION['user']);
+    if (!$found)
+    {
+        $xml->addChild('user', $_SESSION['user']);
 
-    $xmlDocument = new DOMDocument('1.0');
-    $xmlDocument->preserveWhiteSpace = false;
-    $xmlDocument->formatOutput = true;
-    $xmlDocument->loadXML($xml->asXML());
-    $xmlDocument->saveXML();
+        $xmlDocument = new DOMDocument('1.0');
+        $xmlDocument->preserveWhiteSpace = false;
+        $xmlDocument->formatOutput = true;
+        $xmlDocument->loadXML($xml->asXML());
+        $xmlDocument->saveXML();
 
-    if(!$xmlDocument->save('../xml/UserCounter.xml'))
-        die('Error al intentar guardar los datos en el xml.');
+        if(!$xmlDocument->save('../xml/UserCounter.xml'))
+            die('Error al intentar guardar los datos en el xml.');
+    }
 }
 header("Location: Layout.php");
 ?>
